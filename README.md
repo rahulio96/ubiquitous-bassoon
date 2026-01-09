80df605b32317966928b07a5a0831afaf43719d9
rahul <rahulgithub96@gmail.com>
2026-01-08 18:52:04 -0800

    Init backend and api

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# ubiquitous-bassoon
+## Run Frontend
+```bash
+cd ./frontend
+npm install
+npm run dev
+```
+
+## Run Backend
+
+Initialize Python virtual environment first, then:
+```bash
+cd ./backend
+pip install -r ./requirements.txt
+source ./.venv/bin/activate
+fastapi dev main.py
+```
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,44 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 function App() {
 
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  const [text, setText] = useState('');
+
+  const getText = async () => {
+    try {
+      const response = await fetch(
+        `${API_URL}/api/hello`,
+        {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        },
+      );
+
+      if (!response.ok) {
+        console.error('Network response failed:', response.statusText);
+        throw new Error('Network response failed');
+      } else {
+        const data = await response.json();
+        setText(data.message);
+      }
+
+    } catch (error) {
+      setText('Error fetching data');
+    }
+  }
+
+  useEffect(() => {
+    getText();
+  }, []);
+
+
   return (
-    <div className='flex justify-center items-center min-h-screen'>
-      hello
-    </div>
-  )
+    <div className='flex justify-center items-center min-h-screen'>{text}</div>
+  );
 }
 
-export default App
+export default App;
