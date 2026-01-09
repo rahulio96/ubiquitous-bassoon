c083db2b2077d553bb8703663473af54897cefdc
rahul <rahulgithub96@gmail.com>
2026-01-08 18:09:26 -0800

    Init react and tailwind css

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react';
+
+function App() {
+
+  return (
+    <div className='flex justify-center items-center min-h-screen'>
+      hello
+    </div>
+  )
+}
+
+export default App
